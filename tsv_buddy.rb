8b48db2db9ac774dfa5277ce2feaa86bfd20cf3e
Eddie Chung <crc3038@gmail.com>
2017-10-01 11:25:24 +0800

    Solved the homework and all tests pass

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -1,14 +1,44 @@
-# Module that can be included (mixin) to take and output TSV data
+# Module that can be included (mixin) to create and parse TSV data
 module TsvBuddy
+  # @data should be a data structure that stores information
+  #  from TSV or Yaml files. For example, it could be an Array of Hashes.
+  attr_accessor :data
+
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
+    yml_results = []
+    header = []
+    tsv_lines = tsv.split("\n")
+    header = tsv_lines[0].split("\t")
 
+    tsv_lines.shift
+    tsv_lines.each do |line|
+      value = line.split("\t")
+      record = Hash.new(0)
+      header.each_index { |index| record[header[index]] = value[index].chomp }
+      yml_results << record
+    end
+    @data = yml_results
   end
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_tsv
+    tsv_string = ''
+    heading = ''
+    data = @data
 
+    data[0].each_key { |key| heading << key + "\t" }
+    tsv_string << heading.strip
+    tsv_string << "\n"
+
+    0.upto(data.length - 1) do |index|
+      record = ''
+      data[index].each_value { |val| record << val + "\t" }
+      tsv_string << record.strip
+      tsv_string << "\n"
+    end
+    tsv_string
   end
 end
--- yaml_buddy.rb
+++ yaml_buddy.rb
@@ -1,1 +1,11 @@
 # Module that can be included (mixin) to take and output Yaml data
+require 'yaml'
+module YamlBuddy
+    def take_yaml(yml)
+        @data = YAML.load(yml)
+    end
+
+    def to_yaml
+        @data.to_yaml
+    end
+end
